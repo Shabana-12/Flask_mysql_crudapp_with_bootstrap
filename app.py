44d4eb4da14ff4697ec15c45f18d1bb6da0f2bc1
shabana shaikh <86190119+Shabana-12@users.noreply.github.com>
2021-10-19 00:43:06 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -13,17 +13,18 @@ app.config['MYSQL_PASSWORD'] = 'pass@1New'
 app.config['MYSQL_DB'] = 'mypracticedatabase'
 mysql = MySQL(app)
 
+
 @app.route('/')
 def Index():
     cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
- 
+
     cursor.execute('SELECT * FROM employee')
     data = cursor.fetchall()
-  
-    
-    return render_template('list.html', employee= data)
 
-@app.route('/create' , methods = ['GET','POST'])
+    return render_template('list.html', employee=data)
+
+
+@app.route('/create', methods=['GET', 'POST'])
 def create():
     if request.method == 'GET':
         return render_template('create.html')
@@ -34,55 +35,58 @@ def create():
         age = request.form['age']
         address = request.form['address']
         salary = request.form['salary']
+        gender = request.form['gender']
+        etype = request.form['etype']
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         
-        cursor.execute("INSERT INTO employee(name,age,address,salary) VALUES(%s, %s,%s,%s)",
-                           (name,age,address,salary))
+        cursor.execute("INSERT INTO employee(name,age,address,salary,gender,type) VALUES(%s, %s,%s,%s,%s,%s)",
+                           (name,age,address,salary,gender,etype))
         mysql.connection.commit()
         flash("You have successfully added Employee in the list")
         return redirect(url_for('Index'))
 
-@app.route('/detail/<id>', methods = ['POST', 'GET'])
+@app.route('/detail/<id>', methods=['POST', 'GET'])
 def detail(id):
     cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
     cursor.execute('SELECT * FROM employee WHERE id = %s', (id,))
     data = cursor.fetchall()
-    
-    
-    return render_template('detail.html', employee= data[0])
-  
-@app.route('/update/<id>',methods = ['GET','POST'])
+
+    return render_template('detail.html', employee=data[0])
+
+
+@app.route('/update/<id>', methods=['GET', 'POST'])
 def update(id):
     cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
     cursor.execute('SELECT * FROM employee WHERE id = %s', (id,))
     data = cursor.fetchone()
     if request.method == 'POST':
         if data:
-           
+            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
             name = request.form['name']
             age = request.form['age']
             address = request.form['address']
             salary = request.form['salary']
-            cursor.execute("""UPDATE employee SET name = %s,age= %s ,address=%s ,salary=%s WHERE id = %s""", (name,age,address,salary,id,))
+            gender = request.form['gender']
+            etype = request.form['etype']
+            cursor.execute("""UPDATE employee SET name = %s,age= %s ,address=%s ,salary=%s,gender=%s,type=%s WHERE id = %s""",
+                           (name, age, address, salary, gender, etype, id,))
             mysql.connection.commit()
             flash("Employee details updated successfully")
             return redirect(url_for('Index'))
-        
- 
-    return render_template('update.html', employee = data)
 
-@app.route('/delete/<id>', methods = ['GET','POST'])
+    return render_template('update.html', employee=data)
+
+
+@app.route('/delete/<id>', methods=['GET', 'POST'])
 def delete(id):
     cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-   
-   
+
     cursor.execute("DELETE FROM employee WHERE id=%s", (id,))
-    
+
     mysql.connection.commit()
     flash('Employee details Removed Successfully')
     return redirect(url_for('Index'))
-       
-        
+
 
 if __name__ == "__main__":
     app.run()
